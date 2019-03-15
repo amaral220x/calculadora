c5ffdfc34d2b14a8d198aaa8061587fab2b60fd0
Amaral220x <biel.ilha2021@gmail.com>
2019-03-14 21:18:40 -0300

    Um teste

---
+++ main.js
@@ -0,0 +1,6 @@
+function funcaoQuad() {
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("a").value);
+    var c = parseInt(document.getElementById("a").value);
+
+}
